8ace1eff935ef544aae2b968bd16bbd37798a67a
Renato Filho <renatosousafilho@gmail.com>
2020-09-18 09:50:04 -0300

    requisitos

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,22 +1,24 @@
 db.comments.aggregate([
   {
-    $match: {
-      total: { $gt: 150 }
-    }
-  },
-  {
     $group: {
       _id: '$movie_id',
       totalComentarios: { $sum: 1 }
     }
   },
   {
-    $lookup: {
-      from: 'movies',
-      localField: 'movie_id',
-      foreignField: '_id'
+    $match: {
+      totalComentarios: { $gt: 150 }
     }
   },
+  {
+    $lookup: {
+      from: 'movies',
+      localField: '_id',
+      foreignField: '_id',
+      as: 'movie'
+    }
+  },
+  { $unwind: '$movie' },
   { $sort: { totalComentarios: -1, 'movie.title': 1 } },
   { $project: { _id: false, 'filme': '$movie.title', totalComentarios: '$totalComentarios' } }
 ]);
--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -3,12 +3,16 @@ db.movies.aggregate([
     $match: {
       'awards.text': { $regex: /Won \w+ oscar/i },
       released: {
+        $gte: ISODate('2003-01-01'),
         $lte: ISODate('2003-12-31')
+
       },
     }
   },
   { $addFields: { regexAwards: { $regexFind: { input: "$awards.text", regex: /Won (\w+) oscar/i } } } },
   { $unwind: '$regexAwards.captures' },
-  { $addFields: { oscarWins: '$regexAwards.captures' } },
-  { $project: { _id:false, title: true, oscarWins: true} }
+  { $addFields: { oscarWins: { $toInt: '$regexAwards.captures' } } },
+  { $project: { _id: false, title: true, oscarWins: true } },
+  { $sort: { oscarWins: -1 } },
+  { $limit: 1 }
 ]);
